c667cc9f6bdec89c60e01cd1ef6911c2f23848cd
cmarty02 <cristianvmarty@gmail.com>
2024-01-14 15:56:39 +0100

    cambios

--- consumer_rutas.py
+++ consumer_rutas.py
@@ -1,43 +1,45 @@
 from confluent_kafka import Consumer, KafkaError
-import streamlit as st
 import json
+import streamlit as st
 
+###EJECUTAR DESDE CONSOLA CON 'streamlit run consumer_rutas.py'
 # Configuración del consumidor
 config = {
     'bootstrap.servers': 'localhost:9092',
     'group.id': 'python-consumer-group',
-    'auto.offset.reset': 'latest'
+    'auto.offset.reset': 'earliest'
 }
 
-# Función para consumir y mostrar datos en Streamlit
-def consumir_y_mostrar():
-    consumer = Consumer(config)
-    topic = 'rutas'
-    consumer.subscribe([topic])
+# Crear el consumidor
+consumer = Consumer(config)
+topic = 'rutas'
+consumer.subscribe([topic])
 
-    while True:
-        msg = consumer.poll(1.0)  # Espera 1 segundo por nuevos mensajes
+# Configurar la aplicación Streamlit
+st.title("Datos de Kafka en Streamlit")
 
-        if msg is None:
+# Loop para consumir y mostrar datos
+while True:
+    msg = consumer.poll(1.0)
+
+    if msg is None:
+        continue
+    if msg.error():
+        if msg.error().code() == KafkaError._PARTITION_EOF:
             continue
-        if msg.error():
-            if msg.error().code() == KafkaError._PARTITION_EOF:
-                continue
-            else:
-                print(msg.error())
-                break
+        else:
+            print(msg.error())
+            break
 
-        # Decodificar y mostrar el mensaje JSON en Streamlit
-        mensaje_json = json.loads(msg.value().decode('utf-8'))
-        st.write(mensaje_json)
-
-# Aplicación Streamlit
-def main():
-    st.title("Consumidor de Kafka en Streamlit")
-    st.write("Este es un ejemplo simple de un consumidor Kafka integrado en una aplicación Streamlit.")
+    # Obtener el valor del mensaje
+    value = msg.value().decode('utf-8')
     
-    # Llamamos a la función para consumir y mostrar datos
-    consumir_y_mostrar()
+    # Convertir el valor JSON a un diccionario
+    data = json.loads(value)
 
-if __name__ == "__main__":
-    main()
+    # Mostrar datos en Streamlit
+    st.write("Nuevo mensaje recibido:")
+    st.write(data)
+
+# Cerrar el consumidor al salir
+consumer.close()

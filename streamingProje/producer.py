fd97ac49e8b6cad8caaf5ba1dec1d8a682e43b13
cmarty02 <cristianvmarty@gmail.com>
2024-01-21 18:54:41 +0100

    changes

--- streamingProje/producer.py
+++ streamingProje/producer.py
@@ -1,27 +1,64 @@
-from google.cloud import pubsub_v1
 import json
-import random
+import xml.etree.ElementTree as ET
+from google.cloud import pubsub_v1
 import time
 
-def generate_random_name():
-    names = ["Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Harry"]
-    return {"nombre": random.choice(names)}
+# Clase para la publicación en Pub/Sub
+class PubSubProducer:
+    def __init__(self, project_id, topic_name):
+        self.project_id = project_id
+        self.topic_name = topic_name
+        self.publisher = pubsub_v1.PublisherClient()
+        self.topic_path = self.publisher.topic_path(self.project_id, self.topic_name)
 
-def publish_messages():
-    project_id = "dataflow-1-411618"
-    topic_name = "new_topic"
-
-    publisher = pubsub_v1.PublisherClient()
-    topic_path = publisher.topic_path(project_id, topic_name)
-
-    for _ in range(10):  # Enviar 10 nombres aleatorios
-        message = generate_random_name()
+    def publish_message(self, message):
         data = json.dumps(message).encode("utf-8")
-        future = publisher.publish(topic_path, data)
-        print(f"Publicado mensaje: {message}")
+        future = self.publisher.publish(self.topic_path, data)
+        print(f"Publicado mensaje en Pub/Sub: {message}")
+        return future
+
+# Función para cargar coordenadas desde un archivo KML
+def cargar_coordenadas_desde_kml(file_path):
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+    coordinates = []
+
+    for coordinates_tag in root.findall('.//{http://www.opengis.net/kml/2.2}coordinates'):
+        coordinates_text = coordinates_tag.text.strip()
+
+        for coord_set in coordinates_text.split('\n'):
+            coordinates.append(coord_set)
+
+    return coordinates
+
+# Función para convertir coordenadas a formato JSON
+def convertir_a_json(coordinates):
+    coordinates_json = []
+    for index, coord_text in enumerate(coordinates, start=1):
+        lat, lon, alt = [float(coord) for coord in coord_text.split(',')]
+        coordinates_json.append({'index': index, 'latitud': lat, 'longitud': lon})
+
+    return coordinates_json
+
+def main():
+    # Ruta al archivo KML
+    file_path = 'ruta_1.kml'
+
+    # Cargar coordenadas desde el archivo KML
+    coordinates = cargar_coordenadas_desde_kml(file_path)
+
+    # Convertir a formato JSON
+    coordinates_json = convertir_a_json(coordinates)
+
+    # Crear una instancia de la clase PubSubProducer
+    pubsub_producer = PubSubProducer(project_id='dataflow-1-411618', topic_name='coches')
+
+    # Enviar coordenadas a través de Pub/Sub
+    for coord_message in coordinates_json:
+        pubsub_producer.publish_message(coord_message)
         time.sleep(1)  # Esperar 1 segundo entre mensajes
 
-    print("Todos los mensajes han sido enviados.")
+    print("Todos los mensajes han sido enviados a Pub/Sub.")
 
 if __name__ == "__main__":
-    publish_messages()
+    main()
--- streamingProje/store.py
+++ streamingProje/store.py
@@ -4,7 +4,6 @@ from apache_beam.io.gcp.bigquery import WriteToBigQuery
 from apache_beam.io.gcp.internal.clients import bigquery
 import json
 
-# Definir la función decode_message
 def decode_message(msg):
     # Lógica para decodificar el mensaje y cargarlo como JSON
     output = msg.decode('utf-8')
@@ -12,20 +11,25 @@ def decode_message(msg):
     print(f"JSON guardado en BigQuery: {json_data}")
     return json_data
 
-# Definir el esquema de BigQuery
-table_schema = bigquery.TableSchema()
-field_schema = bigquery.TableFieldSchema(name='nombre', type='STRING', mode='NULLABLE')
-table_schema.fields.append(field_schema)
+# Nueva definición del esquema para BigQuery
+new_table_schema = bigquery.TableSchema()
+new_table_fields = [
+    bigquery.TableFieldSchema(name='index', type='INTEGER', mode='NULLABLE'),
+    bigquery.TableFieldSchema(name='latitud', type='FLOAT', mode='NULLABLE'),
+    bigquery.TableFieldSchema(name='longitud', type='FLOAT', mode='NULLABLE')
+]
+new_table_schema.fields.extend(new_table_fields)
+
 
 with beam.Pipeline(options=PipelineOptions(streaming=True)) as p:
     data = (
         p
-        | "LeerDesdePubSub" >> beam.io.ReadFromPubSub(subscription='projects/dataflow-1-411618/subscriptions/new_topic-sub')
+        | "LeerDesdePubSub" >> beam.io.ReadFromPubSub(subscription='projects/dataflow-1-411618/subscriptions/coches-sub')
         | "decodificar_msg" >> beam.Map(decode_message)
         | "escribir" >> WriteToBigQuery(
-              table="dataflow-1-411618:data_test.tabla",
-              schema=table_schema,
-              create_disposition=beam.io.BigQueryDisposition.CREATE_NEVER,
-              write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND
+            table="dataflow-1-411618:blablacar.coordenadas",
+            schema=new_table_schema,
+            create_disposition=beam.io.BigQueryDisposition.CREATE_NEVER,
+            write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND
         )
     )

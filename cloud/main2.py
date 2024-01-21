8d528c4bf8e2e0dafedd76749025fad3c4fceb84
AdrianaC304 <96109282+AdrianaC304@users.noreply.github.com>
2024-01-21 22:21:56 +0100

    save

--- cloud/main2.py
+++ cloud/main2.py
@@ -11,16 +11,28 @@ def decode_message(msg):
 
     return json.loads(output)
 
-    
+# Importa las bibliotecas necesarias
+import apache_beam as beam
+import json
+from apache_beam.options.pipeline_options import PipelineOptions
+
+def decode_message(msg):
+    output = msg.decode('utf-8')
+    return json.loads(output)
+
+def extract_matricula(element):
+    return element.get('Matricula', '')
+
 # Crea el pipeline de Apache Beam
 with beam.Pipeline(options=PipelineOptions(streaming=True)) as p:
 
-     # Lee desde un tema de Pub/Sub
+    # Lee desde un tema de Pub/Sub
     (
         p 
         | "ReadFromPubSub" >> beam.io.ReadFromPubSub(subscription='projects/woven-justice-411714/subscriptions/edema_mda-sub')
         | "decode msg" >> beam.Map(decode_message)
-        | "GroupByMatricula" >> beam.GroupByKey(key=lambda element: element['Matricula'])
+        | "PairWithMatricula" >> beam.Map(lambda element: (extract_matricula(element), element))
+        | "GroupByMatricula" >> beam.GroupByKey()
         | "write" >> beam.io.WriteToBigQuery(
             table="woven-justice-411714:ejemplo.conductores_agrupados",
             schema="Matricula:STRING, Datos:STRING",  # Puedes ajustar el esquema según tus necesidades
@@ -28,3 +40,5 @@ with beam.Pipeline(options=PipelineOptions(streaming=True)) as p:
             write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND
         )
     )
+
+

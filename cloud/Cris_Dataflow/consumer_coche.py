cf74bd124b88deb638a44dc571e4fce7c82b99fe
AdrianaC304 <96109282+AdrianaC304@users.noreply.github.com>
2024-01-22 14:17:34 +0100

    Update consumer_coche.py

--- cloud/Cris_Dataflow/consumer_coche.py
+++ cloud/Cris_Dataflow/consumer_coche.py
@@ -44,31 +44,6 @@ new_table_schema.fields.extend(new_table_fields)
 
 
 
-# Nueva definición del esquema para BigQuery
-new_table_schema_personas = bigquery.TableSchema()
-new_table_fields_personas = [
-    bigquery.TableFieldSchema(name='id_message', type='STRING', mode='NULLABLE'),
-    bigquery.TableFieldSchema(name='persona_id', type='INTEGER', mode='NULLABLE'),
-    bigquery.TableFieldSchema(name='index_msg', type='INTEGER', mode='NULLABLE'),
-    bigquery.TableFieldSchema(name='latitud', type='FLOAT', mode='NULLABLE'),
-    bigquery.TableFieldSchema(name='longitud', type='FLOAT', mode='NULLABLE'),
-    bigquery.TableFieldSchema(name='datetime', type='DATETIME', mode='NULLABLE'),
-    bigquery.TableFieldSchema(name='ruta', type='STRING', mode='NULLABLE')
-]
-new_table_schema_personas.fields.extend(new_table_fields_personas)
-
-
-class FilterMinMaxIndex(beam.DoFn):
-    def process(self, element):
-        _, data = element
-        min_index = min(data, key=lambda x: x['index_msg'])
-        max_index = max(data, key=lambda x: x['index_msg'])
-        return [min_index, max_index]
-    
-
-def print_message(element):
-    print(element)
-    return element
 
 
 with beam.Pipeline(options=PipelineOptions(streaming=True)) as p:
@@ -85,27 +60,3 @@ with beam.Pipeline(options=PipelineOptions(streaming=True)) as p:
         )
     )
 
-
-    #personas:
-    data_personas = (
-            p | "LeerDesdePubSub2" >> beam.io.ReadFromPubSub(subscription='projects/woven-justice-411714/subscriptions/blablacar_personas-sub')
-              #| "PrintMessage" >> beam.Map(print_message)
-              | "decodificar_msg2" >> beam.ParDo(DecodeMessage())
-              | "KeyByPersonId" >> beam.Map(lambda person: (person['id'], person))
-              | "WindowPersons" >> beam.WindowInto(beam.window.FixedWindows(10))  # Ajusta el tamaño de la ventana según sea necesario
-        )
-
-        # Agrupar datos por ID
-    grouped_data = (
-            data_personas
-            | "GroupByKey" >> beam.GroupByKey()
-            | "FilterMinMaxIndex" >> beam.ParDo(FilterMinMaxIndex())
-        )
-
-        # Escribir en BigQuery
-    grouped_data | "WriteToBigQuery" >> beam.io.WriteToBigQuery(
-            table="woven-justice-411714:ejemplo.personas",
-            schema=new_table_schema,
-            create_disposition=beam.io.BigQueryDisposition.CREATE_NEVER,
-            write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND
-        )

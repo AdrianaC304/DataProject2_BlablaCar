587f9c562a9d44ac381e85e61723b8ba81277202
cmarty02 <cristianvmarty@gmail.com>
2024-01-22 23:13:06 +0100

    sctreamlit_ok

--- cloud/Cris_Dataflow/Big_Streamlit.py
+++ cloud/Cris_Dataflow/Big_Streamlit.py
@@ -17,30 +17,33 @@ topic_name= 'coches'
 tabla_name = 'dataflow-1-411618.blablacar.rutas'
 
 ################################################################################################################
-
-
-
-
-
 client = bigquery.Client()
 
 def leer_datos_bigquery(tabla):
     # Agrega comillas inversas alrededor del nombre de la tabla
-    query = f"SELECT coche_id, index_msg, longitud, latitud, datetime, ruta FROM `{tabla}` ORDER BY datetime DESC LIMIT 100"  
+    query = f"SELECT coche_id, index_msg, longitud, latitud, datetime, ruta FROM `{tabla}` ORDER BY index_msg ASC "  
     return client.query(query).to_dataframe()
 
 # Función para crear un mapa de Folium con la ruta y colores diferentes por coche_id
-def crear_mapa_folium(datos):
+def crear_mapa_folium(datos, ruta_seleccionada=None):
     datos.rename(columns={'longitud': 'lon', 'latitud': 'lat'}, inplace=True)
 
-    # Coordenadas del centro del mapa
-    center_coordinates = [datos['lat'].mean(), datos['lon'].mean()]
+    # Filtrar datos por ruta seleccionada
+    if ruta_seleccionada:
+        datos = datos[datos['ruta'] == ruta_seleccionada]
+
+    # Calcular el centro promedio de las coordenadas de las rutas seleccionadas
+    if not datos.empty:
+        center_coordinates = [datos['lat'].mean(), datos['lon'].mean()]
+    else:
+        # Si no hay datos, establecer un centro predeterminado
+        center_coordinates = [39.4699, -0.3763]
 
     # Configuración del tamaño del mapa
     map_width, map_height = 2000, 1200
 
-    # Crear un mapa de Folium
-    mapa_folium = folium.Map(location=center_coordinates, zoom_start=5, control_scale=True, width=map_width, height=map_height)
+    # Crear un mapa de Folium con un estilo simple y gris
+    mapa_folium = folium.Map(location=center_coordinates, zoom_start=5, control_scale=True, width=map_width, height=map_height,  tiles='CartoDB positron')
 
     # Generar colores aleatorios para cada coche_id
     colores = {coche_id: "#{:06x}".format(random.randint(0, 0xFFFFFF)) for coche_id in datos['coche_id'].unique()}
@@ -55,16 +58,16 @@ def crear_mapa_folium(datos):
                       popup=f"Coche ID: {row['coche_id']}, Ruta: {row['ruta']}, Coordenadas: ({row['lat']}, {row['lon']})",
                       icon=folium.Icon(color=color)).add_to(mapa_folium)
 
-        # Crear polilínea si aún no existe para el coche_id
+        # Crear o actualizar polilínea para el coche_id
         if row['coche_id'] not in polilineas_por_coche:
-            polilineas_por_coche[row['coche_id']] = folium.PolyLine(locations=[], color=color)
-
-        # Agregar coordenadas a la polilínea correspondiente al coche_id
-        polilineas_por_coche[row['coche_id']].add_child(folium.Marker(location=[row['lat'], row['lon']]))
+            polilineas_por_coche[row['coche_id']] = []
+        polilineas_por_coche[row['coche_id']].append([row['lat'], row['lon']])
 
     # Agregar polilíneas al mapa
-    for coche_id, polilinea in polilineas_por_coche.items():
-        polilinea.add_to(mapa_folium)
+    for coche_id, coordenadas in polilineas_por_coche.items():
+        color = colores[coche_id]
+        # Evitar que la última coordenada se conecte con la primera
+        folium.PolyLine(locations=coordenadas, color=color).add_to(mapa_folium)
 
     return mapa_folium
 
@@ -72,13 +75,21 @@ if __name__ == "__main__":
     # Nombre de la tabla en BigQuery que quieres leer
     nombre_tabla = tabla_name  # Reemplaza con tu información real
 
-    # Lee los datos de BigQuery y crea el mapa
+    # Lee los datos de BigQuery
     datos = leer_datos_bigquery(nombre_tabla)
-    mapa_folium = crear_mapa_folium(datos)
+
+    # Obtener la lista de rutas únicas
+    rutas_unicas = datos['ruta'].unique()
+
+    # Agregar un slicer (selectbox) para seleccionar la ruta
+    ruta_seleccionada = st.selectbox("Selecciona una ruta:", rutas_unicas)
+
+    # Crea el mapa y la tabla filtrados por la ruta seleccionada
+    mapa_folium = crear_mapa_folium(datos, ruta_seleccionada)
 
     # Muestra la tabla en Streamlit
     st.title("Datos de BigQuery en Streamlit")
-    st.dataframe(datos)
+    st.dataframe(datos[datos['ruta'] == ruta_seleccionada])
 
     # Muestra el mapa en Streamlit
     st.title("Ruta en Mapa desde BigQuery con Folium")

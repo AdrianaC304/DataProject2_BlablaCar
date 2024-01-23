90867c9898a79907b96b52823f720827f3446daa
AdrianaC304 <96109282+AdrianaC304@users.noreply.github.com>
2024-01-23 15:15:07 +0100

    save

--- cloud/PubSub/Asignacion_pandas.py
+++ cloud/PubSub/Asignacion_pandas.py
@@ -1,7 +1,6 @@
-################# Asignación del conductor al coche #################
-
 import pandas as pd
 from google.cloud import bigquery
+from geopy.distance import geodesic
 
 # Configura la credencial de Google Cloud para acceder a BigQuery
 # Asegúrate de haber configurado tu credencial antes de ejecutar el script
@@ -24,9 +23,21 @@ datos_personas = leer_datos_bigquery(tabla_personas)
 # Realizar un merge en función de latitud, longitud y ruta
 datos_combinados = pd.merge(datos_coches, datos_personas, on=['latitud', 'longitud', 'ruta'])
 
-# Filtrar las coordenadas en las que coinciden coches y personas
-coincidencias = datos_combinados[['latitud', 'longitud', 'ruta']].drop_duplicates()
+# Filtrar las coordenadas en las que coinciden coches y personas a menos de 200 metros
+coincidencias = []
 
-# Mostrar las coordenadas en las que coinciden coches y personas
-print("Coordenadas en las que coinciden coches y personas:")
-print(coincidencias)
+for index, row in datos_combinados.iterrows():
+    coord_coche = (row['latitud_x'], row['longitud_x'])
+    coord_persona = (row['latitud_y'], row['longitud_y'])
+
+    distancia = geodesic(coord_coche, coord_persona).meters
+
+    if distancia < 200:
+        coincidencias.append(row[['latitud_x', 'longitud_x', 'ruta']])
+
+# Convertir la lista de coincidencias a un DataFrame
+coincidencias_df = pd.DataFrame(coincidencias, columns=['latitud_x', 'longitud_x', 'ruta'])
+
+# Mostrar las coordenadas en las que coinciden coches y personas a menos de 200 metros
+print("Coordenadas en las que coinciden coches y personas a menos de 200 metros:")
+print(coincidencias_df)
